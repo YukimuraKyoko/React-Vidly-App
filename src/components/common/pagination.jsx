b971ee42e6ab46bee8414ab18925efc30e49b9d8
Tina <TinaNguyen290@yahoo.com>
2021-01-05 15:32:03 -0800

    Implemented Pagination with working page buttons
    Created a new pagination component to render pages depending on itemsCount and pageSize from "props" / or from the movies.jsx component.
    
    In pagination we return an array of pages as a <ul> at the bottom of our movies component in the website.
    
    So far they do render correctly given the a finite number for pageSize, and that the page number won't render if there's not enough items to create a second page.

---
+++ src/components/common/pagination.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import _ from 'lodash'; // underscore library
+
+const Pagination = (props) => {
+    const {itemsCount, pageSize} = props;
+
+    //ex. if pageSize is 4 (max 4 items in each page),
+    //and itemsCount was 15, there will be 4 pages with 3 items on the last page 
+    //ceil() to round up, so that we don't have a double/float value for pages
+    const pagesCount = Math.ceil(itemsCount / pageSize);
+
+    //null check if we have only 1 page then don't render the page buttons
+    if (pagesCount === 1) return null;
+    const pages = _.range(1, pagesCount + 1);
+    // [1...pagesCount].map()
+    return <nav>
+        <ul className="pagination">
+            {pages.map(page =>(
+            <li key={page} className="page-item">
+                <a className="page-link">{page}</a>
+            </li>
+           ))}
+        </ul>
+    </nav>
+
+}
+ 
+export default Pagination;
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import { getMovies } from '../services/fakeMovieService';
 import Like from './common/like'
+import Pagination from './common/pagination';
+
 class Movies extends Component {
     state = {  
-        movies: getMovies()
+        movies: getMovies(),
+        pageSize: 4
     }
 
     handleDelete = (movie) => {
@@ -27,6 +30,10 @@ class Movies extends Component {
         //update the state by replacing the whole movies array
         this.setState({movies});
     };
+
+    handlePageChange = page =>{
+        console.log(page);
+    }
 
     render() { 
         const {length: count} = this.state.movies;
@@ -65,6 +72,9 @@ class Movies extends Component {
                 ))}
                 </tbody>
             </table>
+            <Pagination itemsCount={count} 
+            pageSize={this.state.pageSize}
+            onPageChange={this.handlePageChange} />
             </React.Fragment>)
     }
 }
